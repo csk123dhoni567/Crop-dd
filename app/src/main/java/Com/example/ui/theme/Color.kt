db481507f61bd4cd6c1b7cff7fd3aa7e7da8a8c1
csk123dhoni567 <s06320255@gmail.com>
2026-06-24 15:37:08 +0530

    Add files via upload

---
+++ app/src/main/java/Com/example/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme Botanical Colors
+val BotanicalPrimaryLight = Color(0xFF2E7D32)
+val BotanicalSecondaryLight = Color(0xFF558B2F)
+val BotanicalTertiaryLight = Color(0xFF8D6E63)
+val BotanicalBackgroundLight = Color(0xFFF1F8E9)
+val BotanicalSurfaceLight = Color(0xFFFFFFFF)
+
+// Dark Theme Botanical Colors
+val BotanicalPrimaryDark = Color(0xFF81C784)
+val BotanicalSecondaryDark = Color(0xFFAED581)
+val BotanicalTertiaryDark = Color(0xFFD7CCC8)
+val BotanicalBackgroundDark = Color(0xFF111E12)
+val BotanicalSurfaceDark = Color(0xFF253326)
